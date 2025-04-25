21957565070d75b770a0f0d8d7e5c42c03325ba0
wisnuibroo <wisnuaja0999@gmai.com>
2025-04-25 20:28:13 +0700

    Fix: [WF] tagihan-table

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -1,10 +1,9 @@
 import { Icon } from '@iconify/react';
 
-export default function Search({ placeholder = "Cari..." }) {
+export default function Search( ) {
   return (
-    <div className="w-full md:w-1/4">
-      <form className="flex items-center">
-        <label htmlFor="simple-search" className="sr-only">Search</label>
+    <div className="w-full md:w-1/4 ">
+      <form className="flex items-center"> 
         <div className="relative w-full">
           <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
             <Icon icon="circum:search" className="w-5 h-5 text-gray-500" />
@@ -13,7 +12,7 @@ export default function Search({ placeholder = "Cari..." }) {
             type="search"
             id="simple-search"
             className="block w-full p-2.5 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-white "
-            placeholder={placeholder}
+            placeholder="Cari..."
             required
           />
         </div>
