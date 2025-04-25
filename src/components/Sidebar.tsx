f8de97dc38e51797880956301ab47d939c2e731c
wisnuibroo <wisnuaja0999@gmai.com>
2025-04-25 11:26:47 +0700

    Fix: [WF] sidebar-iconifly

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,6 +1,5 @@
 import { ReactNode } from "react";
-import { MdDashboard, MdHistory, MdSettings, MdMenu, MdClose } from "react-icons/md";
-import { FaMotorcycle, FaComments, FaClipboardList } from "react-icons/fa";
+import { Icon } from "@iconify/react";
 
 type SidebarProps = {
   isOpen: boolean;
@@ -11,15 +10,15 @@ export default function Sidebar({ isOpen, toggleSidebar }: SidebarProps) {
   return (
     <aside className={`bg-[#00ADB5] text-white p-5 space-y-6 transition-all duration-300 ${isOpen ? "w-64" : "w-16"}`}>
       <button onClick={toggleSidebar} className="text-xl focus:outline-none">
-        {isOpen ? <MdClose /> : <MdMenu />}
+        <Icon icon={isOpen ? "mdi:close" : "mdi:menu"} width="24" height="24" />
       </button>
       <div className="flex flex-col space-y-6">
-        <SidebarItem icon={<MdDashboard size={24} />} text="Dashboard" isOpen={isOpen} />
-        <SidebarItem icon={<FaMotorcycle size={24} />} text="Pesanan" isOpen={isOpen} />
-        <SidebarItem icon={<MdHistory size={24} />} text="Riwayat" isOpen={isOpen} />
-        <SidebarItem icon={<FaComments size={24} />} text="Pesan" isOpen={isOpen} />
-        <SidebarItem icon={<FaClipboardList size={24} />} text="Tagihan" isOpen={isOpen} />
-        <SidebarItem icon={<MdSettings size={24} />} text="Pengaturan" isOpen={isOpen} />
+        <SidebarItem icon={<Icon icon="mdi:view-dashboard" width="24" height="24" />} text="Dashboard" isOpen={isOpen} />
+        <SidebarItem icon={<Icon icon="mdi:motorbike" width="24" height="24" />} text="Pesanan" isOpen={isOpen} />
+        <SidebarItem icon={<Icon icon="mdi:history" width="24" height="24" />} text="Riwayat" isOpen={isOpen} />
+        <SidebarItem icon={<Icon icon="mdi:message-text" width="24" height="24" />} text="Pesan" isOpen={isOpen} />
+        <SidebarItem icon={<Icon icon="mdi:clipboard-list" width="24" height="24" />} text="Tagihan" isOpen={isOpen} />
+        <SidebarItem icon={<Icon icon="mdi:cog" width="24" height="24" />} text="Pengaturan" isOpen={isOpen} />
       </div>
     </aside>
   );
