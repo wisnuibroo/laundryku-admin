77730210e2def0c0e78a08e853a6f83048fee6e7
wisnuibroo <wisnuaja0999@gmai.com>
2025-05-03 10:01:56 +0700

    Feat: [WF] riwayat-dashboard

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -16,7 +16,7 @@ export default function Sidebar({ isOpen, toggleSidebar }: SidebarProps) {
       <div className="flex flex-col space-y-6">
         <SidebarItem icon={<Icon icon="mdi:view-dashboard" width="24" height="24" />} text="Dashboard" to="/dashboard" isOpen={isOpen} />
         <SidebarItem icon={<Icon icon="streamline:transfer-motorcycle-solid" width="24" height="24" />} text="Pesanan" to="/pesanan" isOpen={isOpen} />
-        <SidebarItem icon={<Icon icon="mdi:history" width="24" height="24" />} text="Riwayat" to="/" isOpen={isOpen} />
+        <SidebarItem icon={<Icon icon="mdi:history" width="24" height="24" />} text="Riwayat" to="/riwayat" isOpen={isOpen} />
         <SidebarItem icon={<Icon icon="solar:bill-list-bold" width="24" height="24" />} text="Tagihan" to="/tagihan" isOpen={isOpen} />
         <SidebarItem icon={<Icon icon="mdi:cog" width="24" height="24" />} text="Pengaturan" to="/pengaturan" isOpen={isOpen} />
       </div>
