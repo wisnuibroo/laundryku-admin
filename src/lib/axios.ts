4c681ab0d8b8a098a91375af0681ae1aa38561d2
ItsREALKane <akangkenas@gmail.com>
2025-07-28 04:34:25 +0700

    Feat:[KA] Axio Domain

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://127.0.0.1:8000/api',
+  baseURL: 'https://laundryku.rplrus.com/api',
   timeout: 15000, // 15 detik timeout
   headers: {
     'Content-Type': 'application/json',
